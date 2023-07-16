b6bfba8fcba5f57713d6881fa1a2d3edab49f529
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-07-16 23:22:53 +0300

    initial commit 'start spring-boot'

---
+++ src/main/java/com/kino_cms/KinoCmsApplication.java
@@ -0,0 +1,13 @@
+package com.kino_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kino_cms/KinoCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kino_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
