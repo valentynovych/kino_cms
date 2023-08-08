913b621f75e9234887589f0c0f9005dcc507e9fd
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-08-08 22:10:25 +0300

    feedPage -- modify FeedPageController and FeedPageService & add SaveUploadService

--- src/main/java/com/kino_cms/controller/FeedPageController.java
+++ src/main/java/com/kino_cms/controller/FeedPageController.java
@@ -4,6 +4,7 @@ package com.kino_cms.controller;
 import com.kino_cms.entity.FeedPage;
 import com.kino_cms.enums.FeedType;
 import com.kino_cms.service.FeedPageService;
+import com.kino_cms.service.SaveUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -11,14 +12,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Controller
 public class FeedPageController {
@@ -26,6 +25,8 @@ public class FeedPageController {
     private String uploadPath;
     @Autowired
     FeedPageService feedPageService;
+    @Autowired
+    SaveUploadService uploadService;
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     @GetMapping("/admin/edit-feed/{id}")
@@ -46,65 +47,29 @@ public class FeedPageController {
     }
 
     @PostMapping("/admin/edit-feed/{id}")
-    public String saveFeedlPage(@ModelAttribute FeedPage feedPageModel,
-                                  @PathVariable Long id,
-                                  @RequestParam("mainImage1") MultipartFile mainImage,
-                                  @RequestParam("image11") MultipartFile image1,
-                                  @RequestParam("image21") MultipartFile image2,
-                                  @RequestParam("image31") MultipartFile image3,
-                                  @RequestParam("image41") MultipartFile image4,
-                                  @RequestParam("image51") MultipartFile image5) throws IOException {
+    public String saveFeedPage(@ModelAttribute FeedPage feedPageModel,
+                               @PathVariable Long id,
+                               @RequestParam("mainImage1") MultipartFile mainImage,
+                               @RequestParam("image11") MultipartFile image1,
+                               @RequestParam("image21") MultipartFile image2,
+                               @RequestParam("image31") MultipartFile image3,
+                               @RequestParam("image41") MultipartFile image4,
+                               @RequestParam("image51") MultipartFile image5) throws IOException {
+
+        ArrayList<MultipartFile> images = new ArrayList<>(List.of(mainImage, image1, image2, image3, image4, image5));
         Optional<FeedPage> feedPageOptional = feedPageService.getFeedPageById(id);
-        FeedPage feedPage;
-        ArrayList<String> imageList;
+        List<String> fileNamesFromDB;
         if (feedPageOptional.isPresent()) {
-            feedPage = feedPageOptional.get();
-            imageList = new ArrayList<>(6);
-            imageList.add(feedPage.getMainImage());
-            imageList.add(feedPage.getImage1());
-            imageList.add(feedPage.getImage2());
-            imageList.add(feedPage.getImage3());
-            imageList.add(feedPage.getImage4());
-            imageList.add(feedPage.getImage5());
+            fileNamesFromDB = feedPageService.getListImagesFileNameById(id);
         } else {
             feedPageModel.setId(id);
             feedPageModel.setFeedType(FeedType.FEED);
-            imageList = new ArrayList<>(List.of("", "", "", "", "", ""));
+            fileNamesFromDB = new ArrayList<>(List.of("", "", "", "", "", ""));
             feedPageModel.setCreateTime(LocalDateTime.now().format(dateTimeFormatter));
         }
 
-        ArrayList<MultipartFile> images = new ArrayList<>();
-        images.add(mainImage);
-        images.add(image1);
-        images.add(image2);
-        images.add(image3);
-        images.add(image4);
-        images.add(image5);
-
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) {
-            uploadDir.mkdir();
-        }
-        for (int i = 0; i < images.size(); i++) {
-            MultipartFile image = images.get(i);
-            if (image.getOriginalFilename().equals("empty.png")) {
-                File deleteImage = new File(uploadPath + "/" + imageList.get(i));
-                deleteImage.delete();
-                imageList.set(i, null);
-            } else if (!image.isEmpty()) {
-                String uuidFile = UUID.randomUUID().toString();
-                String fileName = uuidFile + "_" + image.getOriginalFilename();
-                image.transferTo(new File(uploadPath + "/" + fileName));
-                imageList.set(i, fileName);
-            }
-        }
-
-        feedPageModel.setMainImage(imageList.get(0));
-        feedPageModel.setImage1(imageList.get(1));
-        feedPageModel.setImage2(imageList.get(2));
-        feedPageModel.setImage3(imageList.get(3));
-        feedPageModel.setImage4(imageList.get(4));
-        feedPageModel.setImage5(imageList.get(5));
+        fileNamesFromDB = uploadService.saveUploadFiles(images, fileNamesFromDB);
+        feedPageModel = feedPageService.updateImagesOnModel(feedPageModel, id, fileNamesFromDB);
 
         feedPageService.saveFeedPage(feedPageModel);
         return "redirect:/admin/view-feeds";
