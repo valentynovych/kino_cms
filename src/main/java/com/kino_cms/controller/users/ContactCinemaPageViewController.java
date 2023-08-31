8edb1cd3369fd2a41187e5314e3f64461bba13df
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-09-01 00:20:38 +0300

    create view for ContactCinemaPageViewController.java

---
+++ src/main/java/com/kino_cms/controller/users/ContactCinemaPageViewController.java
@@ -0,0 +1,23 @@
+package com.kino_cms.controller.users;
+
+import com.kino_cms.entity.ContactPage;
+import com.kino_cms.service.ContactPageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ContactCinemaPageViewController {
+
+    @Autowired
+    ContactPageService contactPageService;
+
+    @GetMapping("/contacts")
+    public ModelAndView viewContacts() {
+        ModelAndView modelAndView = new ModelAndView("user_views/contacts/contactsViewPage");
+        ContactPage contactPage = contactPageService.getContactPageById(1L).get();
+        modelAndView.addObject("contactPage", contactPage);
+        return modelAndView;
+    }
+}
